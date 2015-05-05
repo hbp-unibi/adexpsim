c54b04def015361271f89c7163888196fe9c9c14
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-05-05 02:09:26 +0200

    Show some fancy progress bar, indicating the current state of the exploration

--- src/AdExploration.cpp
+++ src/AdExploration.cpp
@@ -19,7 +19,9 @@
 #include "core/Model.hpp"
 #include "core/Recorder.hpp"
 #include "exploration/Evaluation.hpp"
+#include "utils/Terminal.hpp"
 
+#include <limits>
 #include <iostream>
 #include <fstream>
 
@@ -27,6 +29,7 @@ using namespace AdExpSim;
 
 int main(int argc, char *argv[])
 {
+	Terminal term(true);
 
 	std::ofstream fTime("exploration_time.csv");
 	std::ofstream fCost("exploration_cost.csv");
@@ -34,24 +37,30 @@ int main(int argc, char *argv[])
 
 	WorkingParameters params;
 
-	Evaluation evaluation(3, 1e-3);
+	Evaluation evaluation(3, 5e-3);
 
 	float wOrig = params.wSpike();
 	size_t nY = 0;
-	params.wSpike() = wOrig * 0.03e-6;
-//	for (Val w = 0; w < 0.1e-6; w += 0.001e-6) {
-	for (Val tauL = 0.1e-3; tauL < 10e-3; tauL += 0.1e-3) {
+	params.wSpike() = wOrig * 0.04e-6;
+	for (Val w = 0; w < 0.1e-6; w += 0.0001e-6) {
+		//	for (Val tauL = 0.1e-3; tauL < 10e-3; tauL += 0.1e-3) {
+		// for (Val eTh = 0.0; eTh < 0.05; eTh += 0.0001) {
 		bool first = true;
 		size_t cOk = 0;
-//		for (Val eTh = 0.01; eTh < 0.05; eTh += 0.0001) {
-//		for (Val deltaTh = 0.001e-3; deltaTh < 0.1e-3; deltaTh += 0.001e-3) {
-		for (Val tauE = 0.1e-3; tauE < 200e-3; tauE += 1e-3) {
+		size_t cTotal = 0;
+		Val minCost = std::numeric_limits<Val>::max();
+		//		for (Val eTh = 0.01; eTh < 0.05; eTh += 0.0001) {
+		for (Val deltaTh = 0.0; deltaTh < 10e-3; deltaTh += 0.01e-3) {
+			//		for (Val tauE = 1e-3; tauE < 50e-3; tauE += 0.1e-3) {
+			//		for (Val tauL = 0.1e-3; tauL < 20e-3; tauL += 0.1e-3) {
+			//		for (Val eE = 0.0; eE < 0.4; eE += 0.0002) {
 			// Set the new parameters
-//			params.wSpike = wOrig * w;
-			params.lL() = 1.0 / tauL;
-			params.lE() = 1.0 / tauE;
-//			params.eTh = eTh;
-//			params.deltaTh = deltaTh;
+			params.wSpike() = wOrig * w;
+			params.deltaTh() = deltaTh;
+			//			params.lL() = 1.0 / tauL;
+			//			params.lE() = 1.0 / tauE;
+			//			params.eE() = eE;
+			//params.eTh() = eTh;
 			params.update();
 
 			// Call the evaluation function
@@ -64,17 +73,26 @@ int main(int argc, char *argv[])
 			fCost << std::get<0>(res);
 			fTime << std::get<1>(res);
 			fOk << std::get<2>(res);
+			minCost = std::min(minCost, std::get<0>(res));
 			first = false;
 			cOk = cOk + std::get<2>(res);
+			cTotal++;
 		}
 		fCost << std::endl;
 		fTime << std::endl;
 		fOk << std::endl;
+
+		// Print some fancy progress bar
 		if (nY == 0) {
 			std::cerr << std::endl << " - ";
 		}
 		nY = (nY + 1) % 100;
-		std::cerr << (cOk == 0 ? ' ' : (cOk < 10 ? '.' : (cOk < 100 ? ':' : '|')));
+		Val f1 = Val(cOk) / Val(cTotal);
+		Val f2 = std::max(0.0, (1.0 - std::min(1.0, 0.2 * minCost)) - f1);
+		std::cerr << term.rgb(255 * (1 - f1),
+		                      255 * std::min(1.0, (20.0 * f1 + 0.75 * f2)),
+		                      64 * f2, true) << term.rgb(0, 0, 0, false)
+		          << (cOk > 0 ? "-" : " ") << term.reset();
 	}
 	std::cerr << std::endl;
 }
