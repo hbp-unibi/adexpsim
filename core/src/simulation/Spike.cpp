9ffb19c27b9831a78bccf4782d9a2c880a4d0017
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-05-18 02:24:17 +0200

    Implement SpikeTrain class for the generation of advanced spike trains

--- core/src/simulation/Spike.cpp
+++ core/src/simulation/Spike.cpp
@@ -16,7 +16,10 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <algorithm>
 #include <cmath>
+#include <random>
+#include <vector>
 
 #include "Spike.hpp"
 
@@ -36,5 +39,50 @@ SpikeVec buildInputSpikes(Val xi, Time T, Time t0, Val w)
 	return res;
 }
 
+SpikeTrain::SpikeTrain(const std::vector<Descriptor> &descrs, size_t n, Time T,
+                       Val sigmaT)
+{
+	std::default_random_engine gen;
+
+	// Distribution used to fetch the descriptors
+	std::uniform_int_distribution<> distDescr(0, descrs.size() - 1) ;
+
+	// Iterate over all spike trains that should be generated
+	Time t(TimeType(0));
+	for (size_t i = 0; i < n; i++) {
+		// Fetch a descriptor
+		const Descriptor &descr = descrs[distDescr(gen)];
+
+		// Generate nE + nI spikes
+		std::vector<Spike> spikeGroup;
+		std::normal_distribution<> distT(t.toSeconds(), descr.sigma);
+		for (size_t i = 0; i < descr.nE; i++) {
+			spikeGroup.emplace_back(distT(gen), descr.wE);
+		}
+		for (size_t i = 0; i < descr.nI; i++) {
+			spikeGroup.emplace_back(distT(gen), descr.wI);
+		}
+
+		// Sort the spike group by spike time
+		std::sort(spikeGroup.begin(), spikeGroup.end());
+
+		// Allow no spikes during the spike group itself
+		if (spikeGroup.size() > 1) {
+			ranges.emplace_back(spikeGroup.front().t, 0);
+		}
+
+		// Remember the number of spikes expected after the spike group
+		ranges.emplace_back(spikeGroup.back().t, descr.nOut);
+
+		// Add the spikes to the global spike train
+		spikes.insert(spikes.end(), spikeGroup.begin(), spikeGroup.end());
+
+		// Go to the next timestamp
+		t += std::normal_distribution<>(T.toSeconds(), sigmaT)(gen);
+	}
+
+	// Add a final range at the end
+	ranges.emplace_back(t + T, 0);
+}
 }
 
--- core/src/simulation/Spike.hpp
+++ core/src/simulation/Spike.hpp
@@ -28,6 +28,7 @@
 #ifndef _ADEXPSIM_SPIKE_HPP_
 #define _ADEXPSIM_SPIKE_HPP_
 
+#include <cstdint>
 #include <vector>
 
 #include <utils/Types.hpp>
@@ -47,7 +48,8 @@ struct Spike {
 
 	Spike(Time t, Val w) : t(t), w(w) {}
 
-	friend bool operator<(const Spike &s1, const Spike &s2) {
+	friend bool operator<(const Spike &s1, const Spike &s2)
+	{
 		return s1.t < s2.t;
 	}
 };
@@ -55,18 +57,144 @@ struct Spike {
 using SpikeVec = std::vector<Spike>;
 
 /**
- * Method used to build a set of xi input spikes.
+ * Method used to build a set of N input spikes with uniform spacing.
  *
- * @param xi is the number of input spikes. If a fractional number is given, an
+ * @param N is the number of input spikes. If a fractional number is given, an
  * additional input spike is generated which is scaled by the fractional part
  * of the number.
  * @param T is the delay between the spikes.
  * @param t0 is the time at which the first spike should be generated.
  * @param w is the weight factor with which the spike weights are multiplied.
  */
-SpikeVec buildInputSpikes(Val xi, Time T, Time t0 = 0, Val w = 1);
+SpikeVec buildInputSpikes(Val N, Time T, Time t0 = 0, Val w = 1);
 
+/**
+ * The SpikeTrain class is used to construct input spikes as a set of random
+ * spike trains which are generated from spike train descriptirs. The SpikeTrain
+ * class furthermore generates the ranges in which an output is (or is not)
+ * expected and implements a distance measure which compares the spike
+ */
+class SpikeTrain {
+public:
+	/**
+	 * The Descriptor structure describes a group of input spikes (with
+	 * both inhibitory and excitatory spikes).
+	 */
+	struct Descriptor {
+		/**
+		 * Number of excitatory spikes in the spike group.
+		 */
+		uint16_t nE;
+
+		/**
+		 * Number of inhibitory spikes in the spike group.
+		 */
+		uint16_t nI;
+
+		/**
+		 * Weight factor of the excitatory spikes.
+		 */
+		Val wE;
+
+		/**
+		 * Weight factor of the inhibitory spikes.
+		 */
+		Val wI;
+
+		/**
+		 * Standard deviation of the input spikes in seconds.
+		 */
+		Val sigma;
+
+		/**
+		 * Number of expected output spikes.
+		 */
+		uint16_t nOut;
+
+		/**
+		 * Default constructor.
+		 */
+		Descriptor() : nE(0), nI(0), wE(1.0), wI(1.0), sigma(1.0), nOut(0) {}
+
+		/**
+		 * Constructor for a default excitatory spike group.
+		 */
+		Descriptor(uint16_t nE, uint16_t nOut, Val sigma, Val wE = 1.0)
+		    : nE(nE), nI(0), wE(wE), wI(1.0), sigma(sigma), nOut(nOut)
+		{
+		}
+
+		/**
+		 * Constructor allowing to initialize all members of the Descriptor
+		 * structure.
+		 */
+		Descriptor(uint16_t nE, uint16_t nI, uint16_t nOut, Val sigma, Val wE,
+		           Val wI)
+		    : nE(nE), nI(nI), wE(wE), wI(wI), sigma(sigma), nOut(nOut)
+		{
+		}
+	};
+
+	/**
+	 * The Range descriptor describes how many spikes are expected within a
+	 * range starting at the given time point.
+	 */
+	struct Range {
+		/**
+		 * Start time of this range element.
+		 */
+		Time start;
+
+		/**
+		 * Number of spikes to occur inside this range.
+		 */
+		uint16_t nSpikes;
+
+		Range() : nSpikes(0) {}
+
+		Range(Time start, uint16_t nSpikes) : start(start), nSpikes(nSpikes) {}
+
+		friend bool operator<(const Range &r1, const Range &r2)
+		{
+			return r1.start < r2.start;
+		}
+	};
+
+private:
+	/**
+	 * Generated input spikes.
+	 */
+	SpikeVec spikes;
+
+	/**
+	 * Range descriptor.
+	 */
+	std::vector<Range> ranges;
+
+public:
+	/**
+	 * Constructor of the SpikeTrain class -- calculates a new set of input
+	 * spikes trains.
+	 *
+	 * @param descrs is the set of spike train descriptors from which to
+	 * (randomly) choose.
+	 * @param
+	 */
+	SpikeTrain(const std::vector<Descriptor> &descrs, size_t n, Time T,
+	           Val sigmaT);
+
+	const Time getMaxT() const { return getRanges().back().start; }
+
+	/**
+	 * Returns the generated input spikes.
+	 */
+	const SpikeVec &getSpikes() const { return spikes; }
+
+	/**
+	 * Returns the spike ranges, the last element marks the end time.
+	 */
+	const std::vector<Range> &getRanges() const { return ranges; }
+};
 }
 
 #endif /* _ADEXPSIM_SPIKE_HPP_ */
-
