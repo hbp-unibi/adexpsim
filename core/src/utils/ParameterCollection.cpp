b5e0b7dd90301f2c8b7005b2b6e1c0b64429df31
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-06-03 13:18:30 +0200

    Added ParameterCollection class containing all parameters

---
+++ core/src/utils/ParameterCollection.cpp
@@ -0,0 +1,34 @@
+/*
+ *  AdExpSim -- Simulator for the AdExp model
+ *  Copyright (C) 2015  Andreas Stöckel
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ParameterCollection.hpp"
+
+namespace AdExpSim {
+
+std::vector<std::string> ParameterCollection::modelNames = {"IfCondExp",
+                                                            "AdIfCondExp"};
+
+ParameterCollection::ParameterCollection()
+    : model(ModelType::IF_COND_EXP),
+      train({
+             {3, 1, 1e-3}, {2, 0, 1e-3}, {0, 0, 1e-3},
+            },
+            3, true, 0.033_s)
+{
+}
+}
---
+++ core/src/utils/ParameterCollection.hpp
@@ -0,0 +1,109 @@
+/*
+ *  AdExpSim -- Simulator for the AdExp model
+ *  Copyright (C) 2015  Andreas Stöckel
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file ParameterCollection.hpp
+ *
+ * Contains the ParameterCollection class which contains parameters for both
+ * the model, the exploration and the optimization process.
+ *
+ * @author Andreas Stöckel
+ */
+
+#ifndef _ADEXPSIM_PARAMETER_COLLECTION_HPP_
+#define _ADEXPSIM_PARAMETER_COLLECTION_HPP_
+
+#include <array>
+#include <string>
+#include <vector>
+
+#include <simulation/Parameters.hpp>
+#include <simulation/Spike.hpp>
+
+namespace AdExpSim {
+/**
+ * The ModelType enum defines the model that should be used in the simulation.
+ */
+enum class ModelType: int {
+	/**
+	 * Represents the simple IF_COND_EXP model (integrate & fire conductance
+	 * based with exponential decay).
+	 */
+	IF_COND_EXP = 0,
+
+	/**
+	 * Represents the simple AD_IF_COND_EXP model (integrate & fire conductance
+	 * based with exponential decay and adaptation mechanism).
+	 */
+	AD_IF_COND_EXP = 1
+};
+
+/**
+ * ParameterCollection contains parameters which describe both the neuron model
+ * parameters, the input spike train and information for explorations.
+ */
+struct ParameterCollection {
+	/**
+	 * String list containing the names of the available models. The indices
+	 * in the list correspond to the integer values of the ModelType enum.
+	 */
+	static std::vector<std::string> modelNames;
+
+	/**
+	 * Enum describing the currently used model.
+	 */
+	ModelType model;
+
+	/**
+	 * Holds the current SpikeTrain setup. The SpikeTrain defines the input
+	 * spikes which are being sent to the 
+	 */
+	SpikeTrain train;
+
+	/**
+	 * Holds the current model Parameters.
+	 */
+	Parameters params;
+
+	/**
+	 * Minimum/maximum model Parameters as used for the sliders in the GUI and
+	 * the optimization.
+	 */
+	WorkingParameters min, max;
+
+	/**
+	 * Flags determining whether the corresponding working parameters are 
+	 * optimized in the optimization process or not.
+	 */
+	std::array<bool, WorkingParameters::Size> optimize;
+
+	/**
+	 * Flags determining whether the corresponding working parameters are 
+	 * explored in an offline exploration run.
+	 */
+	std::array<bool, WorkingParameters::Size> explore;
+
+	/**
+	 * Default constructor. Initializes all variables with sane default values.
+	 */
+	ParameterCollection();
+};
+}
+
+#endif /* _ADEXPSIM_PARAMETER_COLLECTION_HPP_ */
+
