610685b68fd29d97376290ba54f5f5fd09550702
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-06-29 00:48:39 +0200

    Implementation of the threaded optimizer which allows parameter optimization for parameters realizable by hardware

--- cli/src/AdExpOptimization.cpp
+++ cli/src/AdExpOptimization.cpp
@@ -16,11 +16,13 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <simulation/HardwareParameters.hpp>
 #include <simulation/Model.hpp>
 #include <simulation/Recorder.hpp>
-#include <exploration/Evaluation.hpp>
-#include <exploration/Simplex.hpp>
+#include <exploration/SingleGroupEvaluation.hpp>
+#include <exploration/Optimization.hpp>
 #include <common/Terminal.hpp>
+#include <io/JsonIo.hpp>
 
 #include <csignal>
 #include <limits>
@@ -30,24 +32,6 @@
 #include <sstream>
 
 using namespace AdExpSim;
-
-std::string printVal(Val x)
-{
-	std::stringstream ss;
-	ss << std::setw(6) << std::setprecision(4) << x;
-	return ss.str();
-}
-
-std::string printCost(Terminal &term, Val cost)
-{
-	Val f1 = std::min<Val>(1.0, 0.2 * cost);
-	Val f2 = 1.0 - std::min<Val>(1.0, 0.1 * cost);
-	Val f3 = 1.0 - std::min<Val>(1.0, 0.5 * cost);
-	std::stringstream ss;
-	ss << term.rgb(f1 * 255, 255 * std::min(1.0, f3 + 0.25 * f2), 64 * f3,
-	               false) << printVal(cost) << term.reset();
-	return ss.str();
-}
 
 /**
  * SIGINT handler. Sets the global "cancel" flag to true when called once,
@@ -63,112 +47,43 @@ void int_handler(int x)
 	cancel = true;
 }
 
-Val estimateW(WorkingParameters params, Val xi)
-{
-	return -log(1 - params.eSpikeEff() / params.eE()) * params.lE() / xi;
-}
-
 int main(int argc, char *argv[])
 {
 	signal(SIGINT, int_handler);
 
-	Terminal term(true);
-
-	Evaluation evaluation(10, 1e-3);
-
-	// Create the initial parameters
+	// Create the initial parameters and the evaluation
 	WorkingParameters params;
+	ModelType model = ModelType::IF_COND_EXP;
+	SingleGroupSpikeData data;  // Default SingleGroupSpikeData (n = 3)
+	SingleGroupEvaluation eval(data, model == ModelType::IF_COND_EXP);
 
-	// Estimate a value for the weight w
-	params.w() = estimateW(params, evaluation.getXi());
+	// Parameter dimensions to optimize (exclude the inhibitory channel
+	// parameters)
+	std::vector<size_t> dims({0, 1, 3, 4, 6, 7, 8, 9, 10, 11, 12});
 
-	std::cout << "Initial value for w: "
-	          << printVal(1000.0 * 1000.0 * params.w() * DefaultParameters::cM)
-	          << "µS" << std::endl;
-
-	// Define the cost function f
-	auto f = [&evaluation](const WorkingParameters &p) -> Val {
-		auto res = evaluation.evaluate(p);
-
-		// Penalize too large time constants (maximum value: 0.2s)
-		const Val errMaxRate =
-		    exp(100 * (1.0 / p.lL() - 0.2)) + exp(100 * (1.0 / p.lE() - 0.2));
-
-		// Penalize too small time constants (minimum value: 1ms)
-		const Val errMinRate =
-		    exp(100 * (p.lL() - 1000)) + exp(100 * (p.lE() - 1000));
-
-		// Penalize too large voltages (maximum value: 200mV)
-		const Val errVolt =
-		    exp(100 * (p.eE() - 0.2)) + exp(100 * (p.eTh() - 0.2));
-
-		// Penalize the time that is being used -- calculate the difference
-		// between the time of the last spike in the spike train and the time
-		// of the first output spike
-		const Val delay =
-		    (std::get<1>(res) - evaluation.getLastSpikeTime()).sec();
-		const Val timePenalty =
-		    10 * std::max<Val>(0.0, std::min<Val>(0.1, delay));
-		return std::get<0>(res) + timePenalty + errMaxRate + errMinRate + errVolt;
-	};
-
-	Simplex<WorkingParameters> simplex(
-	    params, std::vector<size_t>{0, 1, 4, 6, 9, 12}, f);
-
-	SimplexStepResult res;
-	size_t it = 0;
-	do {
-		res = simplex.step(f);
-
-		// Print the best and mean cost#
-		if (it % 10 == 0) {
-			std::cout << "It: " << std::setw(5) << it
-			          << " C: " << printCost(term, res.bestValue) << " ("
-			          << printCost(term, res.meanValue) << ") ";
-
-			if (res.hasNewBest) {
-				std::cout << "↗";
-			} else if (res.reducing) {
-				std::cout << "↘";
-			} else {
-				std::cout << "→";
-			}
-
-			// Print the current parameter set
-			WorkingParameters x = simplex.getSimplex()[0].x;
-			std::cout << " tauL: " << printVal(1000.0 / x.lL()) << "ms";
-			std::cout << " tauE: " << printVal(1000.0 / x.lE()) << "ms";
-			std::cout << " eE: " << printVal(x.eE() * 1000.0) << "mV";
-			std::cout << " eTh: " << printVal(x.eTh() * 1000.0) << "mV";
-			std::cout << " deltaTh: " << printVal(x.deltaTh() * 1000.0) << "mV";
-			std::cout << " w: " << printVal(1000.0 * 1000.0 * x.w() *
-			                                DefaultParameters::cM)
-			          << "µS        ";
-			std::cout << "\r";
-		}
-		it++;
-	} while (!res.done && !cancel);
-
-	auto evalRes = evaluation.evaluate(simplex.getSimplex()[0].x);
-	bool ok = std::get<2>(evalRes);
+	Optimization opt(model, EvaluationResultDimension::SOFT, dims,
+	                 BrainScaleSParameters::inst);
+	std::vector<OptimizationResult> res =
+	    opt.optimize({params}, eval,
+	                 [](size_t nIt, size_t nInput,
+	                    const std::vector<OptimizationResult> &output) -> bool {
+		    std::cout << "nIt: " << nIt << " nInput: " << nInput
+		              << " nOutput: " << output.size();
+		    if (!output.empty()) {
+			    std::cout << " eval: " << output.back().eval;
+		    }
+		    std::cout <<  "        \r";
+		    return !cancel;
+		});
 
 	std::cout << std::endl;
-
-	if (cancel) {
-		std::cout << "Manually aborted optimization" << std::endl;
+	if (!res.empty()) {
+		std::cout << "Final pSoft: " << res.back().eval << std::endl;
+		JsonIo::storePyNNModel(
+		    std::cout, res.back().params.toParameters(DefaultParameters::cM,
+		                                          DefaultParameters::eL),
+		    model);
 	}
-
-	std::cout << "Final Raw Cost: " << std::get<0>(evalRes) << std::endl;
-	std::cout << "Final Time    : " << std::get<1>(evalRes) * 1000 << "ms"
-	          << std::endl;
-	std::cout << "Final Delay   : "
-	          << (std::get<1>(evalRes) -
-	              evaluation.getLastSpikeTime().sec()) *
-	                 1000 << "ms" << std::endl;
-	std::cout << "Heaviside Beh.: "
-	          << (ok ? term.rgb(64, 128, 32, true) + "   OK   "
-	                 : term.rgb(128, 32, 32, true) + "  FAIL  ") << term.reset()
-	          << std::endl;
 
 	return 0;
 }
---
+++ core/src/exploration/Optimization.cpp
@@ -0,0 +1,392 @@
+/*
+ *  AdExpSim -- Simulator for the AdExp model
+ *  Copyright (C) 2015  Andreas Stöckel
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.d.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <algorithm>
+#include <limits>
+#include <mutex>
+#include <deque>
+#include <thread>
+#include <iostream>
+
+#include "Optimization.hpp"
+#include "Simplex.hpp"
+#include "SingleGroupEvaluation.hpp"
+#include "SpikeTrainEvaluation.hpp"
+
+namespace AdExpSim {
+
+// Delta used as minimum difference in evaluation results and vector distances
+static constexpr Val MIN_DIST = 1e-4;
+
+// Maximum value that to-be-optimized inputs may be worse than the currently
+// best output
+static constexpr Val MAX_WORSE = 0.1;
+
+// Maximum change that may occur in an optimization run to consider a parameter
+// set as stable
+static constexpr Val MAX_DIFF = 1e-3;
+
+// Maximum number of iterations in one pass
+static constexpr size_t MAX_IT = 2000;
+
+// Step size of the mixFactor
+static constexpr Val MIX_STEP = 0.1;
+
+/**
+ * The Pool class holds the input and output parameter pool and manages thread-
+ * safe access to these pools.
+ */
+class Pool {
+private:
+	mutable std::mutex mutex;
+
+	/**
+	 * Pushes the given parameter p back onto the given list, but avoids having
+	 * highly similar values on the list.
+	 */
+	template <typename T1, typename T2>
+	bool hasDuplicate(const T1 &list, const T2 &p)
+	{
+		// Calculate the minimum distance to any element already in the list
+		Val minDist = std::numeric_limits<Val>::max();
+		for (const T2 &q : list) {
+			minDist = (p.params - q.params).L2Norm();
+		}
+
+		// Only add the element if the distance is larger than some small
+		// threshold
+		if (minDist < MIN_DIST) {
+			return true;
+		}
+		return false;
+	}
+
+public:
+	/**
+	 * Type used for storing the parameters on the input
+	 */
+	struct InputParameters {
+		WorkingParameters params;
+		Val mixFactor;
+
+		InputParameters() {}
+
+		InputParameters(const WorkingParameters &params, Val mixFactor = 0.0)
+		    : params(params), mixFactor(mixFactor)
+		{
+		}
+	};
+
+	std::deque<InputParameters> input;
+	std::vector<OptimizationResult> output;
+
+	/**
+	 * Creates a new Pool instance and copies the given parameters onto the
+	 * input pool.
+	 */
+	Pool(const std::vector<WorkingParameters> &params)
+	{
+		for (const auto &param : params) {
+			input.emplace_back(param, false);
+		}
+	}
+
+	/**
+	 * Returns a lock_guard instance which synchronizes access to the input and
+	 * output parameters.
+	 */
+	std::unique_lock<std::mutex> lock()
+	{
+		return std::unique_lock<std::mutex>(mutex);
+	}
+
+	/**
+	 * Pops an input parameter from the input pool. This operation atomically
+	 * checks whether an element is available and -- if yes -- returns it.
+	 *
+	 * @return a pair containing a "valid" flag as first value and the actual
+	 * parameter as second value.
+	 */
+	std::pair<bool, InputParameters> popInput()
+	{
+		auto poolLock = lock();
+		if (input.empty()) {
+			return std::make_pair(false, InputParameters());
+		}
+		auto res = std::make_pair(true, input.front());
+		input.pop_front();
+		return res;
+	}
+
+	/**
+	 * Pushes a new parameter onto the input pool, makes sure there are no
+	 * duplicated parameter pairs.
+	 */
+	void pushInput(const WorkingParameters &p, Val eval, bool clampDiscrete)
+	{
+		auto poolLock = lock();
+		const Val bestEval = output.empty() ? 0.0 : output.back().eval;
+		if (bestEval - eval < MAX_WORSE) {
+			const InputParameters ip(p, clampDiscrete);
+			if (!hasDuplicate(input, ip)) {
+				input.emplace_back(ip);
+			}
+		}
+	}
+
+	/**
+	 * Pushes a new output parameter onto the output pool, makes sure there are
+	 * no duplicated parameter pairs.
+	 */
+	void pushOutput(const WorkingParameters &p, Val eval)
+	{
+		auto poolLock = lock();
+
+		// Make sure the evaluation measure is positive
+		if (eval > MIN_DIST) {
+			// Push it onto the output list without duplicates
+			const OptimizationResult opr(p, eval);
+			if (!hasDuplicate(output, opr)) {
+				output.push_back(opr);
+				std::sort(output.begin(), output.end());
+			}
+		}
+	}
+};
+
+std::vector<size_t> Optimization::filterDims(ModelType model,
+                                             const std::vector<size_t> &dims)
+{
+	// If the general AdExp model is given, do nothing
+	if (model == ModelType::AD_IF_COND_EXP) {
+		return dims;
+	}
+
+	// Otherwise filter out the dimensions which are not in the IF_COND_EXP
+	// model
+	std::vector<size_t> res;
+	for (size_t dim : dims) {
+		if (WorkingParameters::inIfCondExp[dim]) {
+			res.push_back(dim);
+		}
+	}
+	return res;
+}
+
+Optimization::Optimization(ModelType model, EvaluationResultDimension evalDim,
+                           const std::vector<size_t> &dims,
+                           const HardwareParameters &hw)
+    : model(model), evalDim(evalDim), dims(filterDims(model, dims)), hw(&hw)
+{
+}
+
+Optimization::Optimization(ModelType model, EvaluationResultDimension evalDim,
+                           const std::vector<size_t> &dims)
+    : model(model), evalDim(evalDim), dims(filterDims(model, dims)), hw(nullptr)
+{
+}
+
+template <typename Evaluation>
+void Optimization::optimizationThread(const Optimization &optimization,
+                                      const Evaluation &eval, Pool &pool,
+                                      std::atomic<bool> &abort,
+                                      std::atomic<size_t> &nIdle,
+                                      std::atomic<size_t> &nIt)
+{
+	// Flag to be passed to the hardware constraints
+	const bool hasHw = optimization.hw;
+	const bool useIfCondExp = optimization.model == ModelType::IF_COND_EXP;
+
+	// Define the cost function f
+	auto f = [&eval, &optimization, hasHw, useIfCondExp](
+	             const WorkingParameters &p) -> Val {
+		// Return the worst possible cost (zero, as all other costs are
+		// negative) if the parameters are not realisable
+		if (!p.valid() ||
+		    (hasHw && !optimization.hw->possible(p, useIfCondExp))) {
+			return 0.0;
+		}
+
+		// Evaluate the parameters, return the negative of the selected
+		// target dimension (the optimization needs a cost and the
+		// evaluation returns a success rate)
+		return -eval.evaluate(p)(optimization.evalDim);
+	};
+
+	// Repeat until the "abort" flag has been set by the calling code
+	while (!abort.load()) {
+		// Fetch an input WorkingParameters set, if no input data is available
+		// just sleep
+		const auto in = pool.popInput();
+		if (!in.first) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(20));
+			continue;
+		}
+
+		// We have work to do!
+		nIdle--;
+
+		// Copy the current WorkingParameters and get the current evaluation
+		// measure
+		const WorkingParameters params = in.second.params;
+		const Val initialEval = f(params);
+
+		// Fetch the current and the next mix factor -- the mix factor is used
+		// to interploate between the forced hardware setup and the
+		// current parameters
+		Val curMf = in.second.mixFactor;
+		Val nextMf = curMf + MIX_STEP;
+		if (hasHw && nextMf > 1.0f) {
+			curMf = 1.0f;
+			nextMf = 0.0f;
+		}
+
+		// Create the simplex algorithm instance, fetch the to-be-optimized
+		// dimensions
+		SimplexStepResult res;
+		Simplex<WorkingParameters> simplex(
+		    params, optimization.getDims(curMf != 0.0), f);
+
+		// Run, abort after MAX_IT iterations or if the simplex indicates it is
+		// done or if the process is manually aborted
+		size_t it = 0;
+		do {
+			// Perform a simplex step
+			res = simplex.step(f);
+
+			// Increment the local and global iteration counter
+			it++;
+			nIt++;
+		} while (!res.done && !abort.load() && it < MAX_IT);
+
+		// Check whether the optimization actually changed the cost -- if yes,
+		// perform hardware parameter clamping and try again
+		const WorkingParameters optimizedParams = simplex.getBest();
+
+		// If a hardware limitation is present, map the optimized values to the
+		// hardware -- then remap them to WorkingParameters. If there is no
+		// HW limitation just add the optimized params.
+		std::vector<WorkingParameters> finalParams;
+		if (hasHw) {
+			std::vector<Parameters> mapped =
+			    optimization.hw->map(optimizedParams, useIfCondExp);
+			for (const Parameters &p : mapped) {
+				finalParams.push_back((optimizedParams * curMf) +
+				                      (WorkingParameters(p) * (1.0f - curMf)));
+			}
+		} else {
+			finalParams.push_back(optimizedParams);
+		}
+
+		// Check whether the parameters should be added to the output or
+		// sent through the pipeline for a second round
+		for (const WorkingParameters &p : finalParams) {
+			// If there has been no substantial change in this optimization run
+			// add the parameters to the output -- otherwise push the optimized
+			// and (possibly mapped) parameters back to the input and use the
+			// next mix factor.
+			const Val eval = f(p);
+			const bool hasSubstantialChange =
+			    fabs(initialEval - eval) > MAX_DIFF;
+			if (!hasSubstantialChange && nextMf == 0.0f) {
+				pool.pushOutput(p, -eval);
+			} else {
+				pool.pushInput(p, -eval, nextMf);
+			}
+		}
+
+		// We're done working, increment the "nIdle" counter
+		nIdle++;
+	}
+}
+
+template <typename Evaluation>
+std::vector<OptimizationResult> Optimization::optimize(
+    const std::vector<WorkingParameters> &params, const Evaluation &eval,
+    ProgressCallback callback) const
+{
+	// Fetch the number of threads to be used
+	size_t nThreads = std::max<size_t>(1, std::thread::hardware_concurrency());
+
+	// Copy the given parameters into the parameter pool
+	Pool pool(params);
+
+	std::atomic<bool> abort(false);       // Flag used to abort all threads
+	std::atomic<size_t> nIdle(nThreads);  // Number of threads idling
+	std::atomic<size_t> nIt(0);           // Number of iterations performed
+
+	// Create a thread for each hardware thread
+	std::vector<std::thread> threads;
+	for (size_t i = 0; i < nThreads; i++) {
+		threads.emplace_back(optimizationThread<Evaluation>, *this, eval,
+		                     std::ref(pool), std::ref(abort), std::ref(nIdle),
+		                     std::ref(nIt));
+	}
+
+	// Wait for all threads to be finished
+	while (true) {
+		{
+			// Abort if all threads idle and the input parameter array is empty
+			// or if the callback returns false
+			auto poolLock = pool.lock();
+			if ((nIdle.load() == nThreads && pool.input.empty()) ||
+			    !callback(nIt.load(), pool.input.size(), pool.output)) {
+				abort.store(true);
+				break;
+			}
+		}
+
+		// Sleep some time before checking again
+		std::this_thread::sleep_for(std::chrono::milliseconds(20));
+	}
+
+	// Wait for all threads to be finished
+	for (auto &thread : threads) {
+		thread.join();
+	}
+
+	// Return the final output parameters
+	return pool.output;
+}
+
+std::vector<size_t> Optimization::getDims(bool clampDiscrete) const
+{
+	if (clampDiscrete) {
+		std::vector<size_t> res;
+		for (size_t dim : dims) {
+			// Do not add the discrete "w" dimension to the result
+			if (dim != WorkingParameters::idx_w) {
+				res.push_back(dim);
+			}
+		}
+		return res;
+	}
+	return dims;
+}
+
+/* Specializations of the "optimize" method. */
+template std::vector<OptimizationResult> Optimization::optimize<
+    SpikeTrainEvaluation>(const std::vector<WorkingParameters> &params,
+                          const SpikeTrainEvaluation &eval,
+                          ProgressCallback callback) const;
+template std::vector<OptimizationResult> Optimization::optimize<
+    SingleGroupEvaluation>(const std::vector<WorkingParameters> &params,
+                           const SingleGroupEvaluation &eval,
+                           ProgressCallback callback) const;
+}
+
