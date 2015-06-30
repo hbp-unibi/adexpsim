abd71f8858ff07c271b98f587ce372a20d39a43d
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-06-30 14:21:43 +0200

    Do not ignore HW ranges

--- io/src/io/JsonIo.cpp
+++ io/src/io/JsonIo.cpp
@@ -80,12 +80,13 @@ void JsonIo::storePyNNSetupESS(std::ostream &os, const Parameters &params,
 {
 	os << "def setup(data, sim):" << std::endl;
 	os << "    return {" << std::endl;
+	os << "        'ignoreHWParameterRanges': False," << std::endl;
 	os << "        'timestep': data.timestep," << std::endl;
 	os << "        'min_delay': data.timestep," << std::endl;
 	os << "        'hardware': sim.hardwareSetup[\"one-hicann\"]," << std::endl;
 	os << "        'useSystemSim': True," << std::endl;
 	os << "        'ignoreDatabase': True," << std::endl;
-	os << "        'ignoreHWParameterRanges': True," << std::endl;
+	os << "        'ignoreHWParameterRanges': False," << std::endl;
 	os << "        'speedupFactor': 10000," << std::endl;
 	os << "        'model': '"
 	   << (model == ModelType::IF_COND_EXP ? "IF_cond_exp"
